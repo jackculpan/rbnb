b8c24f0cbfbfa12d93d536acc9f8c4ebc7bc92e8
jack culpan <jackculpan@me.com>
2019-08-06 12:56:24 +0800

    added pundit authorization onto the application controller, as well as on the bookings controller. Created booking policy and reviews policy

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,23 @@
 class BookingsController < ApplicationController
   before_action :set_pool, :set_user
   def index
-    @bookings = Booking.all
+    @bookings = policy_scope(Booking)
+    authorize @booking
   end
 
   def show
     @booking = find_booking
+    authorize @booking
   end
 
   def new
     @booking = Booking.new
+    authorize @booking
   end
 
   def create
     @booking = Booking.new(booking_params)
+    authorize @booking
     if @booking.save
       redirect_to booking_path(@booking[:id])
     else
@@ -23,10 +27,12 @@ class BookingsController < ApplicationController
 
   def edit
     @booking = find_booking[:id]
+    authorize @booking
   end
 
   def update
     @booking = find_booking
+    authorize @booking
     if @booking.update(booking_params)
       redirect_to booking_path(@booking[:id])
     else
@@ -36,6 +42,7 @@ class BookingsController < ApplicationController
 
   def destroy
     @booking = find_booking
+    authorize @booking
     @booking.destroy
     redirect_to bookings_path(find_user[:user_id])
   end
