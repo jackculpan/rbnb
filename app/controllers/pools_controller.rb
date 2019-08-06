087c57b8bb3233309a5d4291a86efdfbe770607a
Rahul Suvarna <rahulsuvarna18@gmail.com>
2019-08-06 18:01:18 +0800

    made the pool policy

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,21 +1,24 @@
 class PoolsController < ApplicationController
 
   skip_before_action :authenticate_user!, only: :home
-  
+
   def index
     @pools = Pool.all
   end
 
   def show
     @pool = Pool.find(params[:id])
+    authorize @pool
   end
 
   def new
     @pool = Pool.new
+    authorize @pool
   end
 
   def create
     @pool = Pool.new(set_params)
+    authorize @pool
     if @pool.save
       redirect_to pools_path
     else
@@ -25,10 +28,12 @@ class PoolsController < ApplicationController
 
   def edit
     @pool = Pool.find(params[:id])
+    authorize @pool
   end
 
   def update
     @pool = Pool.find(params[:id])
+    authorize @pool
     @restaurant.update(set_params)
     redirect_to pools_path
   end
