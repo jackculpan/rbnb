0f1134552d7b8b73b2ea32026a9b882ee7acba3c
jack culpan <jackculpan@me.com>
2019-08-06 12:05:54 +0800

    created booking index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Pool.create!(location: "surrey", price: 20, number_of_people: 5, description: "description text", photo: "testurl")
+# User.create!(email: "jackculpan@me.com", first_name: "jack", last_name: "culpan")
+# Booking.create!(user_id: 1, pool_id: 1, total_amount: 3, start_date: "01/08/2019", end_date: "08/08/2019")
