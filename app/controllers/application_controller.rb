93fa382f00cfba3079c941b5261c1130629d4304
jack culpan <jackculpan@me.com>
2019-08-06 16:15:58 +0800

    fixed model validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,6 @@ class ApplicationController < ActionController::Base
   private
 
   def skip_pundit?
-    devise_controller? || pools_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,8 @@ puts "Deleting all files"
 User.destroy_all if Rails.env.development?
 Pool.destroy_all if Rails.env.development?
 puts "done delete"
+
 user = User.create!(first_name: 'Jack', last_name: 'Culpan', email: 'bigj@gmail.com', password: '111111')
-Pool.create!(user: user, name: 'Jacks big pool', location: 'Surrey', price: 20, number_of_people: 5, description: 'Good')
+pool = Pool.create!(user_id: user.id, name: 'Jacks big pool', location: 'Surrey', price: 20, number_of_people: 5, description: 'Good', photo: "texttest")
+
+Booking.create!(user_id: user.id, pool_id: pool.id, total_amount: 45, start_date: "20190801", end_date: "20190808")
