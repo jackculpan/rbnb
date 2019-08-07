52aed6b0f6b8395d31607e20242ed43532cf5106
jack culpan <jackculpan@me.com>
2019-08-07 14:32:21 +0800

    added forms and new/edit pages. f.association needs to be set up to link it with a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def dashboard
     @bookings_i_made = current_user.bookings
-    @bookings_i_received = Booking.join(:pools).where(user: current_user)
+    @bookings_i_received = Booking.joins(:pool).where(user: current_user)
     @pools_i_own = Pool.where(user: current_user)
+    authorize @bookings_i_made
+    authorize @bookings_i_received
+    authorize @pools_i_own
+
   end
 end
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,42 @@
+class UserPolicy < ApplicationPolicy
+  # class Scope < Scope
+  #   def resolve
+  #     scope.where(user: user)
+  #   end
+  # end
+
+  def dashboard?
+    is_user_or_admin?
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    is_user_or_admin?
+  end
+
+  def update?
+    is_user_or_admin?
+  end
+
+  def destroy?
+    is_user_or_admin?
+  end
+
+  private
+
+  def is_user_or_admin?
+    record.user == user || user.admin
+  end
+end
+
